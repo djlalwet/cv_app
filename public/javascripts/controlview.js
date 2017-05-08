16614515d187a477d836343cc09bcf194723cbd1
danlalwet <danlalwet>
2017-05-08 17:20:18 +0800

    Fixed responsive design

--- public/javascripts/controlview.js
+++ public/javascripts/controlview.js
@@ -1,65 +1,80 @@
-$('#readmorenokia').click(function(){
-  console.log("read more nokia");
-  $('#infonokia').show();
-  $('#readlessnokia').show();
-  $('#readmorenokia').hide();
-});
+$(function(){
 
-$('#readlessnokia').click(function(){
-  console.log("read less nokia");
-  $('#infonokia').hide();
-  $('#readlessnokia').hide();
-  $('#readmorenokia').show();
-});
+  $('#header').click(function(){
+    $('.btn-nav').click();
+  });
 
-$('#readmorecanon').click(function(){
-  $('#infocanon').show();
-  $('#readlesscanon').show();
-  $('#readmorecanon').hide();
-});
+  $('#readmorenokia').click(function(){
+    console.log("read more nokia");
+    $('#infonokia').show();
+    $('#readlessnokia').show();
+    $('#readmorenokia').hide();
+  });
 
-$('#readlesscanon').click(function(){
-  $('#infocanon').hide();
-  $('#readlesscanon').hide();
-  $('#readmorecanon').show();
-});
+  $('#readlessnokia').click(function(){
+    console.log("read less nokia");
+    $('#infonokia').hide();
+    $('#readlessnokia').hide();
+    $('#readmorenokia').show();
+  });
 
-$('.techimg').mouseenter(function(){
-  $(this).css('opacity', '1');
+  $('#readmorecanon').click(function(){
+    $('#infocanon').show();
+    $('#readlesscanon').show();
+    $('#readmorecanon').hide();
+  });
 
-});
+  $('#readlesscanon').click(function(){
+    $('#infocanon').hide();
+    $('#readlesscanon').hide();
+    $('#readmorecanon').show();
+  });
 
-$('.techimg').mouseleave(function(){
-  $(this).css('opacity', '0.5');
-  $(this).find('.info').hide();
-  $(this).find('.image').show();
-});
+  $('.techimg').mouseenter(function(){
+    $(this).css('opacity', '1');
 
-$('.techimg').click(function(){
-  $(this).find('.info').show();
-  $(this).find('.image').hide();
-});
+  });
 
-$('#hidereferences').click(function(){
-  var referencesinfo = document.getElementById('referencesinfo');
-  var success = referencesinfo.parentNode.removeChild(referencesinfo);
-});
+  $('.techimg').mouseleave(function(){
+    $(this).css('opacity', '0.5');
+    $(this).find('.info').hide();
+    $(this).find('.image').show();
+  });
 
-// The function actually applying the offset
-function offsetAnchor() {
-  if (location.hash.length !== 0) {
-    window.scrollTo(window.scrollX, window.scrollY - 90);
+  $('.techimg').click(function(){
+    $(this).find('.info').show();
+    $(this).find('.image').hide();
+  });
+
+  $('#hidereferences').click(function(){
+    var referencesinfo = document.getElementById('referencesinfo');
+    var success = referencesinfo.parentNode.removeChild(referencesinfo);
+  });
+
+  // The function actually applying the offset
+  function offsetAnchor() {
+    if (location.hash.length !== 0) {
+      window.scrollTo(window.scrollX, window.scrollY - 90);
+    }
   }
-}
 
-// Captures click events of all <a> elements with href starting with #
-$(document).on('click', 'a[href^="#"]', function(event) {
-  // Click events are captured before hashchanges. Timeout
-  // causes offsetAnchor to be called after the page jump.
-  window.setTimeout(function() {
-    offsetAnchor();
-  }, 0);
-});
+  // Captures click events of all <a> elements with href starting with #
+  $(document).on('click', 'a[href^="#"]', function(event) {
+    // Click events are captured before hashchanges. Timeout
+    // causes offsetAnchor to be called after the page jump.
+    window.setTimeout(function() {
+      offsetAnchor();
+    }, 0);
+  });
 
-// Set the offset when entering page with hash present in the url
-window.setTimeout(offsetAnchor, 0);
+  // Set the offset when entering page with hash present in the url
+  window.setTimeout(offsetAnchor, 0);
+     // var navMain = $(".navbar-collapse"); // avoid dependency on #id
+     // // "a:not([data-toggle])" - to avoid issues caused
+     // // when you have dropdown inside navbar
+     // navMain.on("click", "a:not([data-toggle])", null, function () {
+     //     navMain.collapse('hide');
+     // });
+
+ });
+
